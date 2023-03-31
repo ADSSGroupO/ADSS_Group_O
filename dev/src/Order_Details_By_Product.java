02348fbd659cea2d6d58997fae6779d5a6c6a260
noa-ye <noayesha@post.bgu.ac.il>
2023-03-31 13:35:44 +0300

    heyyyyy 1

---
+++ dev/src/Order_Details_By_Product.java
@@ -0,0 +1,9 @@
+public class Order_Details_By_Product {
+    private String product_code;
+    private String product_name;
+    private int amount;
+    private int list_price; //the price in the price list
+    private int discount;
+    private double final_price;
+
+}
