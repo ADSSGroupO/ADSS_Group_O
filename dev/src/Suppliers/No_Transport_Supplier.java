9f8aebc454bd8aa83f9a69f6303e80508fe24389
lioraft <lioraftabi@gmail.com>
2023-04-04 21:46:48 +0300

    updating classes 4/4

--- dev/src/Suppliers/No_Transport_Supplier.java
+++ dev/src/Suppliers/No_Transport_Supplier.java
@@ -1,5 +1,7 @@
 package Suppliers;
 
+import java.util.Date;
+
 public class No_Transport_Supplier extends Supplier {
     // description of class: this is a class that represents the suppliers of superli, who can't deliver their orders, and needs delivery assistance.
     // it inherits the class supplier. in addition to parent class attributes, it has an address, which is the address that the orders will be picked up from.
@@ -13,4 +15,12 @@ public class No_Transport_Supplier extends Supplier {
     // getter and setter for address
     public String getAddress(){return address;}
     public void setAddress(String newAddress) {address=newAddress;}
+
+
+    // function that returns the next shipping date - because we arrange delivery, the date is today by default (needs to be further integrated with other modules)
+    @Override
+    public Date getNextShippingDate() {
+        Date date = new java.util.Date();
+        return date;
+    }
 }
