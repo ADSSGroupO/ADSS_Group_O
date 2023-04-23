dc4584e6cf5d4cb0e6ebaec74ab1cff6dbe57b91
lioraft <lioraftabi@gmail.com>
2023-04-23 17:06:42 +0300

    made minor change in code to check if branches merged properly

--- dev/src/Suppliers/Main.java
+++ dev/src/Suppliers/Main.java
@@ -4,7 +4,7 @@ import Suppliers.PresentationLayer.CLI;
 
 public class Main {
     public static void main(String[] args) {
-        // create new CLI
+        // create new CLI instance
         CLI cli = new CLI();
         // start menu
         cli.printMenu();
